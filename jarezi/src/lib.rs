16b669675a3fbb67549c4978119cacbcd853dc15
Jarett Dunn <jarettrsdunn+git@gmail.com>
2022-09-22 16:09:54 -0300

    real shit don't haunt me

--- jarezi/src/lib.rs
+++ jarezi/src/lib.rs
@@ -1,15 +1,5 @@
-use {
-    anchor_lang::{
-        solana_program::system_program,
-    }
-};
+pub mod utils;
 
-use borsh::{BorshSerialize, BorshDeserialize};
-use std::num::NonZeroU64;
-use serum_dex::matching::*;
-
-use serum_dex::state::*;
-use serum_dex::instruction::*;
 use {
     anchor_lang::{
         prelude::*,
@@ -18,7 +8,6 @@ use {
 };
 anchor_lang::declare_id!("EMXJxUgMWD9uKiS2mtYCRBmKbGN9sHsDwMUnhNvWJR1X");
 pub const PREFIX: &str = "jarezi";
-
 #[program]
 pub mod jarezi {
 
@@ -26,115 +15,96 @@ pub mod jarezi {
 
 
 
-   
-    pub  fn set_market(ctx: Context<SetMarket>
-    )  -> Result<()> {
-        let stuff = ctx.accounts.stuff;
-        stuff.proxy = ctx.accounts.proxy.key();
-        stuff.market = ctx.accounts.market.to_account_info();
-        stuff.open_orders = ctx.accounts.open_orders.to_account_info();
-        stuff.request_queue = ctx.accounts.request_queue.to_account_info();
-        stuff.event_queue = ctx.accounts.event_queue.to_account_info();
-        stuff.market_bids = ctx.accounts.bids.to_account_info();
-        stuff.market_asks = ctx.accounts.asks.to_account_info();
-        stuff.order_payer_token_account = ctx.accounts.order_payer_token_account.key();
-        stuff.open_orders_authority = ctx.accounts.prediction_market_account.to_account_info();
-        stuff.coin_vault = ctx.accounts.coin_vault.to_account_info();
-        stuff.pc_vault = ctx.accounts.pc_vault.to_account_info();
-        stuff.token_program = ctx.accounts.token_program.to_account_info();
-        stuff.rent = ctx.accounts.rent.to_account_info();
-        stuff.dex_program = ctx.accounts.dex_program.to_account_info();
-        Ok(())
+    pub fn join_jarezi<'a, 'b, 'c, 'info>(
+        ctx: Context<'a, 'b, 'c, 'info, JoinJareZi<'info>>,
+    ) -> Result<u8> {
+        let jarezi_instance = &mut ctx.accounts.jarezi_instance;
+        let jares = &mut ctx.accounts.jares;
+        let payer = &ctx.accounts.payer;
+
+as None
+        jarezi_instance.token_types_added = jarezi_instance
+            .token_types_added
+            .checked_add(1)
+            .ok_or(ErrorCode::NumericalOverflowError)?;
+
+        let now_ts = Clock::get().unwrap().unix_timestamp;
+
+        if jarezi_instance.lastthousand == 0 {
+            jarezi_instance.lastthousand = (now_ts + 1000) as i64;
+        }
+    if jares.lastplay  < now_ts - 10 || jares.lastplay > now_ts - 2  {// 5 > 20 -10 = 10 no
+        jares.disqualified = true; 
+        msg!("disq4");
+    
     }
+        if jares.lastplay == 0  {
+            jares.disqualified = false;
+            jares.nice = 0;
+            msg!("disq2");
+        }
+            jares.nice = jares.nice.checked_add(1).ok_or(ErrorCode::NumericalOverflowError)?;
+            msg!("canplay");
 
-  pub  fn new_order2(
-    ctx: Context<NewOrder2>,
-        side: Side,
-        limit_price: u64,
-        coin_amount: u64,
-        order_type: OrderType,
-    )  -> Result<()> {
-        let stuff = &ctx.accounts.stuff;
-        let (price_lots, coin_lots, pc_lot_size) = {
-            let market = MarketState::load(&stuff.market, &stuff.proxy, true);
-            (limit_price.checked_mul(market.unwrap().coin_lot_size).unwrap()
-                        .checked_div(market.unwrap().pc_lot_size).unwrap()
-                        .checked_div(BASE as u64).unwrap(), 
-            coin_amount.checked_div(market.unwrap().coin_lot_size).unwrap(),
-            market.unwrap().pc_lot_size)
-        };
-        let max_native_pc_qty = price_lots.checked_mul(coin_lots).unwrap().checked_mul(pc_lot_size).unwrap();
-        // Client order id is only used for cancels. Not used here so hardcode.
-        let client_order_id = 0;
-        // Limit is the dex's custom compute budge parameter, setting an upper
-        // bound on the number of matching cycles the program can perform
-        // before giving up and posting the remaining unmatched order.
-        let limit = 65535;
-        // wat
-       // let short_name = stuff.prediction_market_account.short_name.as_ref().clone();
-       // let seeds = &[
-         //   short_name.trim_ascii_whitespace(),
-        //    &[stuff.prediction_market_account.nonce],
-        //];
+        if (now_ts) as i64 > jarezi_instance.lastthousand - 10 && !jares.disqualified && !jarezi_instance.bonus && jares.nice > 9 {
+            jarezi_instance.bonus = true;
+            return Ok(1 as u8);
+        }
+        if (now_ts) as i64 > jarezi_instance.lastthousand && !jares.disqualified && jares.nice > 1 {
+            msg!("winnawinnachickems");
 
-        let seeds = &[];
-        let signer =  &ctx.accounts.signer;
-        let mut dex_accs = vec![
-            AccountMeta::new(*stuff.market.key, false),
-            AccountMeta::new(*stuff.open_orders.key, false),
-            AccountMeta::new(*stuff.request_queue.key, false),
-            AccountMeta::new(*stuff.event_queue.key, false),
-            AccountMeta::new(*stuff.market_bids.key, false),
-            AccountMeta::new(*stuff.market_asks.key, false),
-            AccountMeta::new(stuff.order_payer_token_account.clone(), false),
-            AccountMeta::new_readonly(stuff.prediction_market_account.to_account_info().key(), true), // signer?
-            AccountMeta::new(*stuff.coin_vault.key, false),
-            AccountMeta::new(*stuff.pc_vault.key, false),
-            AccountMeta::new_readonly(*stuff.token_program.key, false),
-            AccountMeta::new_readonly(*stuff.rent.key, false),
-        ];
-        let ctx2 = CpiContext::new_with_signer(stuff.dex_program.clone(), dex_accs, &[]);
-        // if let Some(referral) = referral {
-        //     ctx = ctx.with_remaining_accounts(vec![referral]);
-        // }
-        new_order (
-            ctx2,
-            side.into(),
-            NonZeroU64::new(price_lots).unwrap(),
-            NonZeroU64::new(coin_lots).unwrap(),
-            NonZeroU64::new(max_native_pc_qty).unwrap(),
-            SelfTradeBehavior::DecrementTake,
-            order_type,
-            client_order_id,
-            limit,
-        );
-        Ok(())
+            jares.token_types_removed = jarezi_instance.token_types_removed;
+            jarezi_instance.lastthousand = (now_ts + 1000) as i64;
+            jares.nice = 0;
+            if now_ts > jarezi_instance.lastplay  {
+                msg!("becomewinna");
+                jarezi_instance.lastplay = now_ts; 
+                jarezi_instance.winning = payer.key();
+                jares.lastplay = now_ts; 
+                
+                
+            }
+            if (jares.token_types_removed < jarezi_instance.token_types_removed) && jares.disqualified {
+                jares.disqualified = false;
+                msg!("disq1");
+                jares.nice = 0;
+                jares.token_types_removed = jarezi_instance.token_types_removed;
+            }
+            if (now_ts) as i64 > jarezi_instance.lastthousand - 10 && !jares.disqualified && !jarezi_instance.bonus && jares.nice > 9 {
+                jarezi_instance.bonus = true;
+    
+                return   Ok(2 as u8);
+            }
+        if (now_ts) as i64 > jarezi_instance.lastthousand  && !jares.disqualified && jares.nice > 1 { 
+            msg!("winnawinnachickems");
+
+            jares.token_types_removed = jarezi_instance.token_types_removed;
+            jarezi_instance.lastthousand = (now_ts + 1000) as i64;
+            jares.nice = 0;
+    
+    
+            return   Ok(3 as u8);
+        }
+        
     }
+        if now_ts > jarezi_instance.lastplay  {
+            msg!("becomewinna");
+            jarezi_instance.lastplay = now_ts; 
+            jarezi_instance.winning = payer.key();
+            jares.lastplay = now_ts; 
+            
+            
+        }
 
-  
-
+        if (jares.token_types_removed < jarezi_instance.token_types_removed) && jares.disqualified {
+            jares.disqualified = false;
+            msg!("disq1");
+            jares.nice = 0;
+            jares.token_types_removed = jarezi_instance.token_types_removed;
+        }
+        Ok(4 as u8)
+    }
 }
-
-
-
-#[account]
-pub struct Stuff<'info> {
-    token_program: AccountInfo<'info>,
-    proxy: Pubkey,   
-    market: AccountInfo<'info>,
-        open_orders: AccountInfo<'info>,
-        request_queue: AccountInfo<'info>,
-        event_queue: AccountInfo<'info>,
-        market_bids: AccountInfo<'info>,
-        market_asks: AccountInfo<'info>,
-        order_payer_token_account: Pubkey,
-        prediction_market_account: AccountInfo<'info>,
-        open_orders_authority: AccountInfo<'info>,
-        coin_vault: AccountInfo<'info>,
-        pc_vault: AccountInfo<'info>,
-        rent: AccountInfo<'info>,
-        dex_program: AccountInfo<'info>
-  }
 
 #[derive(Accounts)]
 pub struct JoinJareZi<'info> {
@@ -155,39 +125,31 @@ pub struct JoinJareZi<'info> {
     rent: Sysvar<'info, Rent>,
 }
 
-#[derive(Accounts)]
-pub struct NewOrder2 <'info>{
-    stuff: Account<'info, Stuff<'info>>,
-    #[account(mut)]
-    signer: Signer<'info>,
+#[account]
+pub struct JareZi {
+    token_types_added: u8,
+    token_types_removed: u8,
+    jarezi_instance: Pubkey,
+    lastthousand: i64,
+    jares2: Pubkey,
+    winning: Pubkey,
+    lastplay: i64,
+    bonus: bool
+    
 }
-#[derive(Accounts)]
-pub struct InitStuff<'info> {
+#[account]
 
-#[account(init, payer = payer, space = 300)]
-stuff: Account<'info, Stuff<'info>>,
-#[account(mut)]
-payer: Signer<'info>,
-#[account(address = system_program::ID)]
-system_program: AccountInfo<'info>,
+ struct Jares {
+    lastplay: i64,
+    disqualified: bool,
+    token_types_removed: u8,
+    nice: u8
 }
-#[derive(Accounts)]
-pub struct SetMarket<'info> {
-  #[account(mut)]
-  stuff: Account<'info, Stuff<'info>>,
-    proxy: UncheckedAccount<'info>,
-  market: UncheckedAccount<'info>,
-  open_orders: UncheckedAccount<'info>,
-  request_queue: UncheckedAccount<'info>,
-  event_queue: UncheckedAccount<'info>,
-  bids: UncheckedAccount<'info>,
-  asks: UncheckedAccount<'info>,
-  prediction_market_account: UncheckedAccount<'info>,
-  order_payer_token_account: UncheckedAccount<'info>,
-  open_orders_authority:UncheckedAccount<'info>, // stays pubkey
-  coin_vault: UncheckedAccount<'info>,
-  pc_vault: UncheckedAccount<'info>,
-  token_program: UncheckedAccount<'info>,
-  rent: UncheckedAccount<'info>,
-  dex_program: UncheckedAccount<'info>
+#[error_code]
+pub enum ErrorCode {
+    #[msg("hm")]
+    GenericError,
+    #[msg("numbers")]
+    NumericalOverflowError,
+    
 }
